707db72570031fb0d9a8282577637a29405fdc02
GabrielDebug <you@example.com>
2026-02-16 18:11:41 -0300

    Create Soma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main()
+        {
+            Soma();
+        }
+    static void Soma()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite o primeiro valor: ");
+        float v1 = float.Parse(Console.ReadLine()); 
+            
+        Console.WriteLine("Digite o Segundo valor: ");
+        float v2 = float.Parse(Console.ReadLine());
+
+        float resultado = v1 + v2;
+        Console.WriteLine($"O resultado da soma é {resultado} ");
+    } 
+    }
+}
